052ecb2b1f28f2b76e80fb2f59363ad8d9b13a77
Ankit <ankit.kknjs@gmail.com>
2022-11-09 16:39:49 +0530

    9-11-22

--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
 // Server API Root URL
-<<<<<<< HEAD
-npm export const SERVER_ROOT_URL = 'http://localhost:3001';
-=======
 export const SERVER_ROOT_URL = 'http://localhost:3001';
->>>>>>> c0fec31a6def3aa37acfb6175de77effb7995935
 //export const SERVER_ROOT_URL = 'http://cms.ijetem.com';
